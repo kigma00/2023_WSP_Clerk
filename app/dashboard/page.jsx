e93ed152c076583b5fb82d64370828de0a1b41a4
kigma00 <kimga000408@gmail.com>
2023-09-26 10:42:31 +0900

    WebSecurePrograming

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,16 @@
+/*rafce*/
+
+import React from 'react'
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-5">Dashboard</h1>
+      <p className="mb-5">
+        Welcome to Dashboard. This page show login-host info
+      </p>
+    </div>
+  )
+}
+
+export default DashboardPage
